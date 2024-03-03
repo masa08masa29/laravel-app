94a330a859cfa09a5cb8cf25cc8a7ca64eaf7c60
masa08masa29 <148183238+masa08masa29@users.noreply.github.com>
2024-03-03 17:43:19 +0900

    Revert "Sort"

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,7 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-
     
 </head>
 <body>
